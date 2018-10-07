87eb6512d06f68f0b3df1654fb8106e34efd36a8
unknown <ykim306@gmail.com>
2018-10-07 15:43:19 -0400

    Ex 05_02 Completed

--- app/src/main/java/com/example/plainolnotes3/ui/NotesAdapter.java
+++ app/src/main/java/com/example/plainolnotes3/ui/NotesAdapter.java
@@ -1,13 +1,16 @@
 package com.example.plainolnotes3.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.plainolnotes3.EditorActivity;
 import com.example.plainolnotes3.R;
 import com.example.plainolnotes3.database.NoteEntity;
 
@@ -15,6 +18,8 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static com.example.plainolnotes3.utilities.Constants.NOTE_ID_KEY;
 
 public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> {
 
@@ -38,6 +43,14 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> 
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         final NoteEntity note = mNotes.get(position);
         holder.mTextview.setText(note.getText());
+        holder.mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, EditorActivity.class);
+                intent.putExtra(NOTE_ID_KEY, note.getId());
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -48,6 +61,9 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> 
     public class ViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.note_text)
         TextView mTextview;
+
+        @BindView(R.id.fab)
+        FloatingActionButton mFab;
 
         public ViewHolder(View itemView) {
             super(itemView);
